338bab6af72e6c12c4e4907a0500d37dd45b2082
famouswolfe <famouswolfe@gmail.com>
2019-08-02 14:34:38 -0600

    request/body validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register users
 // @access  Public
-router.get("/", (req, res) => res.send("User route"));
+router.post(
+  "/",
+  [
+    check("name", "Your name is required")
+      .not()
+      .isEmpty(),
+    check("email", "A valid email is requied").isEmail(),
+    check(
+      "password",
+      "Your password must contain at least 7 characters"
+    ).isLength({
+      min: 7
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User route");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect database
 connectDB();
 
+// initialize middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running"));
 
 //Define our routes
